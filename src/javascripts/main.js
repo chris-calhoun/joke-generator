0f92b87d32647390b4bc12ca3d8fb94b5d6e1f34
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-10-31 12:31:27 -0500

    created card to contain jokes.

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,18 @@
 import '../styles/main.scss';
 
 const init = () => {
-  $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
+  $('#app').append('<div id="joke-card"></div>');
+  $('#joke-card').append(`<div class="card">
+  <div class="card-header">
+    Jokes
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <button type="button" class="btn btn-primary">Get Joke</button>
+    <button type="button" class="btn btn-primary">Get Punchline</button>
+  </div>
+</div>`);
 };
 
 init();
