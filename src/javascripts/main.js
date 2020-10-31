5ca8b30c5816cf15eec90001509b348c8883e9a1
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-10-31 12:49:09 -0500

    made successful call to joke api.

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,16 +1,25 @@
 import '../styles/main.scss';
+import JokeData from './data';
 
 const init = () => {
+  JokeData.getJoke().then((response) => {
+    if (response.length) {
+      console.warn(response);
+    }
+  });
+
   $('#app').append('<div id="joke-card"></div>');
   $('#joke-card').append(`<div class="card">
   <div class="card-header">
     Jokes
   </div>
   <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <h5 class="card-title">Joke Generator</h5>
+    <h5 class="card-title">Punchline</h5>
     <button type="button" class="btn btn-primary">Get Joke</button>
+    <p>Joke</p>
     <button type="button" class="btn btn-primary">Get Punchline</button>
+    <p>Punchline</p>
   </div>
 </div>`);
 };
